1dd868f2c2333a859b633d3e052a44653d640b23
André de Paula Terceiro <andre@terceiro.com.br>
2025-07-09 17:34:02 -0300

    feat: doing until class 17

--- andreterceiro/src/index.js
+++ andreterceiro/src/index.js
@@ -41,17 +41,39 @@ async function playerRaceEngine(character1, character2) {
 
         if (block == "RETA") {
             totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
-            totalTestSkill2 = diceResult2 + character1.VELOCIDADE;
+            totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
             await logRollResult(player1.NOME, "velocidade", diceResult1, character1.VELOCIDADE);            
             await logRollResult(player2.NOME, "velocidade", diceResult2, character2.VELOCIDADE);            
        } else if (block == "CURVA") {
             totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
-            totalTestSkill2 = diceResult2 + character1.MANOBRABILIDADE;
+            totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
             await logRollResult(player1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);            
             await logRollResult(player2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
         } else {
             let powerResult1 = diceResult1 + character1.PODER;
             let powerResult2 = diceResult2 + character2.PODER;
+            console.log(`${character1.NOME} confrontou com ${character2.NOME}`);
+            await logRollResult(player1.NOME, "poder", diceResult1, character1.PODER);            
+            await logRollResult(player2.NOME, "poder", diceResult2, character2.PODER);            
+        
+            if (powerResult1 > powerResult2) {
+                if (character2.PONTOS > 0) {
+                    character2.PONTOS--;
+                    console.log(`${character2.NOME} perdeu 1 ponto`);
+                } else {
+                    console.log(`${character2.NOME} não perdeu pontos pois estava com zero pontos`);
+                }
+
+            } else if (powerResult2 > powerResult1) {
+                if (character1.PONTOS > 0) {
+                    character1.PONTOS--;
+                    console.log(`${character1.NOME} perdeu 1 ponto`);
+                } else {
+                    console.log(`${character1.NOME} não perdeu pontos pois estava com zero pontos`);
+                }
+            } else {
+                console.log("Confronto empatado! Nenhum ponto foi perdido");
+            }
         }
 
         // Verificando o vencedor
@@ -62,6 +84,8 @@ async function playerRaceEngine(character1, character2) {
         } else if (totalTestSkill2 > totalTestSkill1) {
             console.log(`${character2.NOME} marcou 1 ponto`);
             character2.PONTOS++;
+        } else if (totalTestSkill1 > 0) { // Para evitar o caso de block == "CONFRONTO"
+            console.log("Rodada empatada");
         }
     }
 }
