fbb83113286c2a8b0d9bd39047820223e28b9186
André de Paula Terceiro <andre@terceiro.com.br>
2025-07-09 14:23:34 -0300

    feat: doing until class 12

--- andreterceiro/src/index.js
+++ andreterceiro/src/index.js
@@ -21,9 +21,38 @@ async function rollDice() {
 async function playerRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
         console.log(`Rodada: ${round}`);
+
+        // Sortear bloco
+        let block = getRandomBlock();
+        console.log(`Bloco sorteado: ${block}\n`);
+
+        // rolar os dados
+        let diceResult1 = await rollDice();
+        let diceResult2 = await rollDice();
+
+        // teste de habilidade
+        let totalTestSkill1 = 0; 
+        let totalTestSkill2 = 0;
+    }
+}
+
+function getRandomBlock() {
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO";
+            break;
     }
 
-    // Sortear bloco
+    return result;
 }
 
 (async function main() {
