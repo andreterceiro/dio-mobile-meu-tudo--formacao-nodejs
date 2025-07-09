cef055b08bbb011fb5a0d21009dc132a0198d2b9
André de Paula Terceiro <andre@terceiro.com.br>
2025-07-09 18:07:44 -0300

    feat: small adjusts to make the game more readable

--- andreterceiro/src/index.js
+++ andreterceiro/src/index.js
@@ -91,14 +91,15 @@ async function playerRaceEngine(character1, character2) {
 }
 
 async function declareWinner(character1, character2) {
+    console.log("\n\n*******************************************")
     console.log("Resultado final:");
     console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`)
     console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`)
     
     if (character1.PONTOS > character2.PONTOS) {
-        console.log(`\n ${character1.NOME} venceu a corrida! Parabéns!`);
+        console.log(`\n${character1.NOME} venceu a corrida! Parabéns!`);
     } else if (character1.PONTOS < character2.PONTOS) {
-        console.log(`\n ${character2.NOME} venceu a corrida! Parabéns!`);
+        console.log(`\n${character2.NOME} venceu a corrida! Parabéns!`);
     } else {
         console.log("A corrida terminou empatada")
     }
